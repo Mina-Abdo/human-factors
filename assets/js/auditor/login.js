13c10154dab912c97c4df6886a99da4525fe088f
LinaLiu89 <liulina1004a@gmail.com>
2024-09-04 18:27:50 +0930

    Implement auditor features

---
+++ assets/js/auditor/login.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById('login');
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    //perform input validation
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    if(email && password){
+        window.location.href = "auditorindex.html";
+    }else{
+        let errorMessage = document.createElement('div');
+        errorMessage.id = 'errorMessage';
+        errorMessage.textContent = "Invalid email or password";
+        errorMessage.style.color = 'red';
+        errorMessage.style.marginTop = '15px';
+        errorMessage.style.fontSize = '20px';
+        errorMessage.style.textAlign = 'center';
+        document.getElementById('login').appendChild(errorMessage);
+    }
+})
